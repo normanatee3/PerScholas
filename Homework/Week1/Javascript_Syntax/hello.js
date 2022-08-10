1cac7d3d15aecfe160ebc8a45b80587f39b791df
normanatee3 <normankennedytaylor@gmail.com>
2022-08-09 20:28:24 -0400

    updated secondary homework

---
+++ Homework/Week1/Javascript_Syntax/hello.js
@@ -0,0 +1,48 @@
+// Exercise 1
+
+// console.log("Hello, World!")
+
+// Exercise 2
+
+// let myApples = 5
+// let myOranges = 3
+// let myFruit = myApples + myOranges
+// console.log(myFruit)
+
+// Exercise 3
+// note: a bit confused by the language
+
+// let myApples = 5
+// let myOranges = 3
+// var myKiwi = 7
+// var stolenKiwi = 2
+// const myPear = 9
+// let myFruit = myApples + myKiwi - stolenKiwi + myOranges + myPear
+// console.log(myFruit)
+
+// Exercise 4
+
+// let myOne = 2
+// let myTwo = 5
+// let myThree = 4
+// let myFour = 11
+// let myFive = 3
+// let mySix = 8
+// let mySeven = 21
+// let myEight = 35
+// let myNine = 14
+// let myTen = 1
+// let fiveFirst = (myOne + myTwo + myThree + myFour + myFive) / 5
+// let fiveLast = (mySix + mySeven + myEight + myNine + myTen) / 5
+// let myAverage = (fiveFirst + fiveLast) / 2
+// console.log(fiveFirst, fiveLast, myAverage)
+
+// Exercise 5
+
+let avFirst = (4 + 10 + 22 - 30 + 55) / 5
+let avSecond = (68 + 7 - 22 + 9 +100) / 5
+let avLast = (avFirst + avSecond) 
+console.log(avLast)
+
+// returns 22.2999 instead of 22.3
+// caused by float inaccuracy?

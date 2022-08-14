33eed27ad93032516746b16fe8cbbb44aac5412a
normanatee3 <normankennedytaylor@gmail.com>
2022-08-14 16:55:31 -0400

    hi

---
+++ Labs/Week1/Day5/Area/areaFunction.js
@@ -0,0 +1,8 @@
+
+const computeArea = (width, height) => {
+    let area = width*height
+    return `The area of a rectangle with a width of ${width} and a height of ${height} is ${area} square units`
+}
+
+console.log(computeArea(4,7));
+

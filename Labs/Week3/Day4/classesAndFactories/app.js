e9b285181fc28726dddc48c6ecb0bc64d39c6762
normanatee3 <normankennedytaylor@gmail.com>
2022-08-26 09:30:42 -0400

    created timmys story

--- Labs/Week3/Day4/classesAndFactories/app.js
+++ Labs/Week3/Day4/classesAndFactories/app.js
@@ -57,3 +57,31 @@ class Person{
         this.bankAccount-= hamster.getPrice()
     }
 }
+
+const timmy = new Person ('Timmy') 
+
+for(let i = 0; i < 5; i++){
+    timmy.ageUp()
+}
+for(let i = 0; i < 5; i++){
+    timmy.eat()
+}
+for(let i = 0; i < 5; i++){
+    timmy.exercise()
+}
+for(let i = 0; i < 9; i++){
+    timmy.ageUp()
+}
+const gus = new Hamster('Gus')
+gus.owner = 'Timmy'
+timmy.buyHamster(gus)
+
+for(let i = 0; i < 15; i++){
+    timmy.ageUp()
+}
+for(let i = 0; i < 2; i++){
+    timmy.eat()
+}
+for(let i = 0; i < 2; i++){
+    timmy.exercise()
+}

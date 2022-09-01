64933c8a506f06009d39fec666c95f560af27c52
Norman Taylor <68932268+normanatee3@users.noreply.github.com>
2022-09-01 13:04:33 -0400

    drumpad

---
+++ Labs/Week4/Day3/drumPad/app.js
@@ -0,0 +1,109 @@
+let sound13 = new Audio ('/sounds/808 Bass Drop.wav')
+let sound14 = new Audio ('/sounds/Kick 001 Basic.wav')
+let sound15 = new Audio ('/sounds/TomHip Latin Kit 050.wav')
+let sound16 = new Audio ('/sounds/Bell 001 Revved.wav')
+let sound9 = new Audio ('/sounds/Hit Gold 1.wav')
+let sound10 = new Audio ('/sounds/Hit Gold 3.wav')
+let sound11 = new Audio ('/sounds/Chant Hey 003.wav')
+let sound12 = new Audio ('/sounds/Scream 1.wav')
+let sound5 = new Audio ('/sounds/Clap 005.wav')
+let sound6 = new Audio ('/sounds/Snap 11.wav')
+let sound7 = new Audio ('/sounds/Snare rollHip Latin Kit 036.wav')
+let sound8 = new Audio ('/sounds/TriangleHip Latin Kit 060.wav')
+let sound1 = new Audio ('/sounds/Crash 001.wav')
+let sound2 = new Audio ('/sounds/HiHat Closed 005 Tight and Wide.wav')
+let sound3 = new Audio ('/sounds/HiHat Open 001.wav')
+let sound4 = new Audio ('/sounds/ShakerHip Latin Kit 074.wav')
+// let play1;
+// let play2;
+// let play3;
+// let play4;
+// let play5;
+// let play6;
+// let play7;
+// let play8;
+// let play9;
+// let play10;
+// let play11;
+// let play12;
+// let play13;
+// let play14;
+// let play15;
+// let play16;
+// let sounds = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16]
+// let plays = [play1, play2, play3, play4, play5, play6, play7, play8, play9, play10, play11, play12, play13, play14, play15, play16]
+
+
+// for(i=1; i<=16; i++){
+//     plays[i] = document.querySelector(`.drumPad${i}`)
+//     plays[i].addEventListener("click", () =>{
+//         sounds[i].play()
+//     })
+// }
+
+
+
+let play1 = document.querySelector('.drumPad1')
+play1.addEventListener("click", () =>{
+    sound1.play()
+})
+let play2 = document.querySelector('.drumPad2')
+play2.addEventListener("click", () =>{
+    sound2.play()
+})
+let play3 = document.querySelector('.drumPad3')
+play3.addEventListener("click", () =>{
+    sound3.play()
+})
+let play4 = document.querySelector('.drumPad4')
+play4.addEventListener("click", () =>{
+    sound4.play()
+})
+let play5 = document.querySelector('.drumPad5')
+play5.addEventListener("click", () =>{
+    sound5.play()
+})
+let play6 = document.querySelector('.drumPad6')
+play6.addEventListener("click", () =>{
+    sound6.play()
+})
+let play7 = document.querySelector('.drumPad7')
+play7.addEventListener("click", () =>{
+    sound7.play()
+})
+let play8 = document.querySelector('.drumPad8')
+play8.addEventListener("click", () =>{
+    sound8.play()
+})
+let play9 = document.querySelector('.drumPad9')
+play9.addEventListener("click", () =>{
+    sound9.play()
+})
+let play10 = document.querySelector('.drumPad10')
+play10.addEventListener("click", () =>{
+    sound10.play()
+})
+let play11 = document.querySelector('.drumPad11')
+play11.addEventListener("click", () =>{
+    sound11.play()
+})
+let play12 = document.querySelector('.drumPad12')
+play12.addEventListener("click", () =>{
+    sound12.play()
+})
+let play13 = document.querySelector('.drumPad13')
+play13.addEventListener("click", () =>{
+    sound13.play()
+})
+let play14 = document.querySelector('.drumPad14')
+play14.addEventListener("click", () =>{
+    sound14.play()
+})
+let play15 = document.querySelector('.drumPad15')
+play15.addEventListener("click", () =>{
+    sound15.play()
+})
+let play16 = document.querySelector('.drumPad16')
+play16.addEventListener("click", () =>{
+    sound16.play()
+})

e266952a38397cf68bd9805c8b313179de440903
Norman Taylor <68932268+normanatee3@users.noreply.github.com>
2022-09-13 09:04:30 -0400

    gitignore

---
+++ Labs/Week6/Day2/node-practice/days-of-week.js
@@ -0,0 +1,7 @@
+module.exports.weekdays =
+	['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+module.exports.getWeekday = function(dayNo) {
+	if (dayNo < 0 || dayNo > 6) dayNo = 0;
+	return this.weekdays[dayNo];
+};
---
+++ Labs/Week6/Day2/node-practice/script.js
@@ -0,0 +1,4 @@
+let daysOfWeek = require('./days-of-week');
+
+let day = daysOfWeek.getWeekday(7);
+console.log(day);

001831d64e8d1dcf5a8672fd8c639ff0166b3e1e
Norman Taylor <68932268+normanatee3@users.noreply.github.com>
2022-09-15 19:16:20 -0400

    codebugs

--- Homework/Week6/Day3/pass-it-around/server.js
+++ Homework/Week6/Day3/pass-it-around/server.js
@@ -6,22 +6,32 @@ app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res)=>{
-    res.send("<h1> 99 Bottles of beer on the wall </h1>" + "<h1>99 Bottles of beer</h1>" + `<h1><a href="/98">ya take one down, pass it around</a></h1>` + `<h1>98 Bottles of beer on the wall.</h1>`)
+    res.send("<h1> 99 game breaking bugs in the code </h1>" + "<h1>99 game breaking bugs</h1>" + `<h1><a href="/98">ya take one down, patch it around</a></h1>` + `<h1>98 game breaking bugs in the code.</h1>`)
 })
 app.get('/0', (req, res)=>{
-    res.send("<h1> 0 Bottles of beer on the wall </h1>" + "<h1>0 Bottles of beer</h1>" + `<h1><a href="/">ya go to the store, and buy some more</a></h1>` + `<h1>99 Bottles of beer on the wall.</h1>`)
+    
+    res.send("<h1> 0 game breaking bugs in the code </h1>" + "<h1>0 game breaking bugs</h1>" + `<h1><a href="/">ya go to the server, and break your game further</a></h1>` + `<h1>99 game breaking bugs in the code.</h1>`)
 })
 
 app.get('/:bottle', (req, res)=>{
-    const minus = parseInt(req.params.bottle)
-    const newBottles = (minus - 1).toString()
+    const oldBottles = parseInt(req.params.bottle)
+    let newBottles = (oldBottles - 1).toString()
+    let bug = 0
+    let random = Math.random()
+    if(random > 0.8){
+        bug += 1
+    }else{
+        bug = 0
+    }
+    if(bug === 1){
+        newBottles = (oldBottles + Math.floor(Math.random()*10)).toString()
+    }
     res.send(
-        `<h1>`+ req.params.bottle + ` Bottles of beer on the wall </h1>`
-        + `<h1>`+ req.params.bottle + ` Bottles of beer</h1>`
-        + `<h1><a href="/` + newBottles + `">ya take one down, pass it around</a></h1>` 
-        + `<h1>` + newBottles + ` Bottles of beer on the wall.</h1>`)
+        `<h1>`+ req.params.bottle + ` game breaking bugs in the code </h1>`
+        + `<h1>`+ req.params.bottle + ` game breaking bugs</h1>`
+        + `<h1><a href="/` + newBottles + `">ya take one down, patch it around</a></h1>` 
+        + `<h1>` + newBottles + ` game breaking bugs in the code.</h1>`)
 })
-
 
 
 

4b900d4f1307f11d723413f47052638be757c2cb
Norman Taylor <68932268+normanatee3@users.noreply.github.com>
2022-09-15 20:49:54 -0400

    my server is set up and running

---
+++ Labs/Week6/Day4/pokemon_app/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to the Pokemon App!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, (req, res) =>{
+    console.log(`listening on 3000`);
+})
